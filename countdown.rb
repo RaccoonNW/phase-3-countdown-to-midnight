6e390e7deb02181bf0ebc87790aa66a36048f24f
Gabriel <shalitmontagne.g@gmail.com>
2022-01-27 18:01:26 -0700

    Completed Assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+require 'pry'
 
-def countdown
+def countdown i
+    while i > 0
+        puts "#{i} SECOND(S)!"
+        i -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep i
+    while i > 0
+        sleep 1
+        puts "#{i} SECOND(S)!"
+        i -= 1
+    end
+    "HAPPY NEW YEAR!"
+    
 end
